2673ca5a5f0c55346e884942c540879aa3d34064
FrGe2016 <fgenestcvdl@gmail.com>
2021-11-20 09:27:09 -0500

    Menu creation2

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/francois/Test_catkin_ws_fg3/src/waypoints_list_tool
-# Build directory: /home/francois/Test_catkin_ws_fg3/src/waypoints_list_tool/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "actionlib;geometry_msgs;move_base_msgs;rospy;smach;std_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lwaypoints_list_tool".split(';') if "-lwaypoints_list_tool" != "" else []
-PROJECT_NAME = "waypoints_list_tool"
-PROJECT_SPACE_DIR = "/usr/local"
-PROJECT_VERSION = "0.0.1"
